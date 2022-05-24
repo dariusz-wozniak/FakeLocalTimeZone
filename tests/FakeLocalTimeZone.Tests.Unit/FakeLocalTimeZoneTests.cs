4234ff2a80414a19eab11ad643681c5177276092
Dariusz Woźniak <dariusz.wozniak@columbusglobal.com>
2022-05-24 16:58:25 +0200

    test: assertion for TimeZoneInfo.Local.Id

--- tests/FakeLocalTimeZone.Tests.Unit/FakeLocalTimeZoneTests.cs
+++ tests/FakeLocalTimeZone.Tests.Unit/FakeLocalTimeZoneTests.cs
@@ -18,10 +18,7 @@ namespace FakeLocalTimeZone.Tests.Unit
         {
             using (new FakeLocalTimeZone(TimeZoneInfo.FindSystemTimeZoneById("UTC+12")))
             {
-                var localDateTime = new DateTime(2020, 12, 31, 23, 59, 59, DateTimeKind.Local);
-                var utcDateTime = TimeZoneInfo.ConvertTimeToUtc(localDateTime);
-
-                Assert.That(utcDateTime, Is.EqualTo(localDateTime.AddHours(-12)));
+                Assert.That(TimeZoneInfo.Local.Id, Is.EqualTo("UTC+12"));
             }
         }
 
